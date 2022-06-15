5f57214de4a07e51f38ada97c46461b106c2c985
Vlad Shalya <61494265+CoringPlay@users.noreply.github.com>
2022-06-15 17:05:07 +1000

    Add files via upload

---
+++ poke_1.js
@@ -0,0 +1,39 @@
+// var status = function (response) {
+//   if (response.status !== 200) {
+//     return Promise.reject(new Error(response.statusText))
+//   }
+//   return Promise.resolve(response)
+// }
+// var json = function (response) {
+//   return response.json()
+// }
+
+//   let s = document.getElementById('image');
+//   fetch(`https://api.catboys.com/img`)
+//     .then((response) => {
+//       document.getElementById('images').src = response.url;
+//     });
+
+const button = document.querySelector(".btn");
+const image = document.querySelector(".image__cover");
+const art = document.querySelector(".image__artist")
+const link = "https://api.catboys.com/img";
+
+async function fetchHandler() {
+  try {
+    const response = await fetch(link);
+    const data = await response.json();
+    image.src = data.url;
+    art.text = data.artist;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+button.addEventListener("click", () => {
+  let isLoaded = image.complete;
+
+  if (isLoaded) {
+    fetchHandler();
+  }
+});
