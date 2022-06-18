be221aede6ed6de6a9322756c6f64c275cb43e6a
Vlad Shalya <61494265+CoringPlay@users.noreply.github.com>
2022-06-18 15:35:54 +1000

    Cleaning js

--- poke_1.js
+++ poke_1.js
@@ -1,30 +1,16 @@
-// var status = function (response) {
-//   if (response.status !== 200) {
-//     return Promise.reject(new Error(response.statusText))
-//   }
-//   return Promise.resolve(response)
-// }
-// var json = function (response) {
-//   return response.json()
-// }
-
-//   let s = document.getElementById('image');
-//   fetch(`https://api.catboys.com/img`)
-//     .then((response) => {
-//       document.getElementById('images').src = response.url;
-//     });
-
 const button = document.querySelector(".btn");
 const image = document.querySelector(".image__cover");
-const link = "https://api.catboys.com/img";
+const art = document.querySelector(".image__artist")
+const link = "https://api.waifu.pics/sfw/neko";
 
 async function fetchHandler() {
   try {
     const response = await fetch(link);
     const data = await response.json();
     image.src = data.url;
+    art.text = data.artist;
   } catch (error) {
-    console.log('Возникли проблемы',error.message);
+    console.log(error);
   }
 }
 
